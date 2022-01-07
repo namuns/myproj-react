ae1f7f8aa34843293cd50510460bb28521f276ae
namuns <skymsn214@gmail.com>
2022-01-07 15:56:58 +0900

    Alert 컴포넌트 예시/스타일 추가 및 Review 별점 map에 key 지정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import TopNav from 'components/TopNav';
 import Login from 'pages/accounts/Login';
+import Components from 'pages/examples/Components';
 import Profile from 'pages/accounts/Profile';
 import ReviewList from 'pages/reviews/ReviewList';
 import { Route, Routes, Navigate } from 'react-router-dom';
+import { Component } from 'react/cjs/react.production.min';
 
 import './App.css';
 
@@ -15,6 +17,7 @@ function App() {
         <Route path="/accounts/login/" element={<Login />} />
         <Route path="/accounts/profile/" element={<Profile />} />
         <Route path="/reviews/" element={<ReviewList />} />
+        <Route path="/examples/components/" element={<Components />} />
       </Routes>
     </div>
   );
