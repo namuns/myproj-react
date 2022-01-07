1795f9ec799e1611d8d1e13ce47394e21bf09b82
namuns <skymsn214@gmail.com>
2022-01-07 14:06:19 +0900

    axios 활요하여 reviewList api 호출

--- src/pages/reviews/ReviewList.jsx
+++ src/pages/reviews/ReviewList.jsx
@@ -1,4 +1,26 @@
+import Axios from 'axios';
+import { useEffect } from 'react';
+
 function ReviewList() {
+  useEffect(() => {
+    refetch();
+  }, []);
+
+  const refetch = () => {
+    const url = 'http://127.0.0.1:8000/shop/api/reviews/';
+    // Promise 객체
+    Axios.get(url)
+      .then((response) => {
+        console.group('정상응답');
+        console.log(response);
+        console.groupEnd();
+      })
+      .catch((error) => {
+        console.group('에러응답');
+        console.log(error);
+        console.groupEnd();
+      });
+  };
   return (
     <div>
       <h2>ReviewList</h2>
