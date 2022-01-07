dccc1e96752c103812e5ae6fc4eef0d0c2c801bf
namuns <skymsn214@gmail.com>
2022-01-07 14:58:31 +0900

    list 조회 시 상탯값 처리 Review 컴포넌트로 ReviewList 컴포넌트 구성

--- src/pages/reviews/ReviewList.jsx
+++ src/pages/reviews/ReviewList.jsx
@@ -1,35 +1,56 @@
 import Axios from 'axios';
 import DebugStates from 'components/DebugStates';
 import { useState, useEffect } from 'react';
+import Review from 'components/Review';
 
 function PageReviewList() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
   const [reviewList, setReviewList] = useState([]);
   useEffect(() => {
     refetch();
   }, []);
 
   const refetch = () => {
+    setLoading(true);
+    setError(null);
+
     const url = 'http://127.0.0.1:8000/shop/api/reviews/';
     // Promise 객체
     Axios.get(url)
       .then(({ data }) => {
-        console.group('정상응답');
-        console.log(data);
-        console.groupEnd();
         setReviewList(data);
       })
       .catch((error) => {
-        console.group('에러응답');
-        console.log(error);
-        console.groupEnd();
+        console.error(error);
+        setError(error);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   };
   return (
     <div>
       <h2>ReviewList</h2>
+
+      {loading && <div>Loading...</div>}
+      {error && <div>통신중 오류 발생!</div>}
+
+      <button
+        onClick={() => refetch()}
+        className="bg-yellow-300 hover:bg-red-300"
+      >
+        새로고침
+      </button>
+
+      <div className="">
+        {reviewList.map((review) => (
+          <Review key={review.id} review={review} />
+        ))}
+      </div>
+
       <hr />
-      <DebugStates reviewList={reviewList} />
-      <pre>{JSON.stringify(reviewList, null, 2)}</pre>
+      <DebugStates loading={loading} error={error} reviewList={reviewList} />
     </div>
   );
 }
